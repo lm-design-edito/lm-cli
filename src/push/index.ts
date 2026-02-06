2f0050b20c5a7fe757a88a13c24a970cb4c09455
lm-maxfab <fabas@lemonde.fr>
2026-02-07 00:19:00 +0100

    src/push quick fix

--- src/push/index.ts
+++ src/push/index.ts
@@ -20,7 +20,6 @@ program
     files: string | undefined,
     rest: string[] | undefined,
     options: { pull?: boolean },
-    command
   ) => {
     console.log({
       remote,
@@ -30,7 +29,6 @@ program
       rest,
       options
     })
-
     if (options.pull) {
       const pullRes = await spawner(
         `Pulling from ${remote}/${branch}...`,
@@ -51,70 +49,16 @@ program
       const commitRes = await spawner(
         `Commiting with message "${message}"...`,
         'git',
-        ['commit', '-m', `${message}`]
+        ['commit', '-m', message]
       )
       if (!commitRes.success) throw process.exit(1)
     }
     await promptContinue('push now?', true)
     await spawner(
       `Pushing to ${remote}/${branch}`,
-      'echo',
-      ['fake', 'git', 'push', remote, branch]
+      'git ',
+      ['push', remote, branch, ...rest ?? []]
     )
-      
-    // spawn(
-    //   'git',
-    //   ['-c', 'color.ui=always', 'pull', ...pullOptions],
-    //   { stdio: 'pipe' }
-    // ).on('exit', code => process.exit(code ?? 0))
-    
-
-    // if (files !== undefined) spawn(
-    //   'git',
-    //   ['-c', 'color.ui=always', 'add', ...args],
-    //   { stdio: 'inherit' }
-    // ).on('exit', code => process.exit(code ?? 0))
-
   })
 
 program.parse(process.argv)
-
-// program
-//   .name('@design-edito/push')
-//   .description('Shorthand for git push with arguments forwarding')
-//   .allowUnknownOption(true)
-//   .option('-p, --pull', 'pull before pushing')
-//   .argument('[remote]', 'name of the remote source')
-//   .argument('[branch]', 'name of the branch')
-//   .argument('[message]', 'message of the commit')
-//   .argument('[files]', 'files to add')
-//   .option('--flag', 'some optopn')
-//   .action((
-//     remote: string | undefined,
-//     branch: string | undefined,
-//     message: string | undefined,
-//     files: string | undefined,
-//     rest: string,
-//     options: string | undefined
-//   ) => {
-//     // spawn(
-//     //   'git',
-//     //   ['-c', 'color.ui=always', 'add', ]
-//     // )
-
-//     console.log({
-//       remote,
-//       branch,
-//       message,
-//       files,
-//       rest,
-//       options
-//     })
-
-//     // spawn(
-//     //   'git',
-//     //   ['-c', 'color.ui=always', 'push', ...args],
-//     //   { stdio: 'inherit' }
-//     // ).on('exit', code => process.exit(code ?? 0))
-//   })
-
