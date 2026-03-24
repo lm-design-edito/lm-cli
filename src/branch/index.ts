c4eb5cc44457bea34d69711d1b5bbae1baa564de
lm-maxfab <fabas@lemonde.fr>
2026-03-24 09:36:37 +0100

    branch: created

---
+++ src/branch/index.ts
@@ -0,0 +1,18 @@
+import { spawn } from 'child_process'
+import { program } from 'commander'
+
+program
+  .name('@design-edito/branch')
+  .description('Shorthand for git branch with arguments forwarding (defaults to ".")')
+  .allowUnknownOption(true)
+  .arguments('[args...]')
+  .action((args: string[]) => {
+    const finalArgs = args.length > 0 ? args.join(' ') : '.'
+    spawn(
+      'git',
+      ['-c', 'color.ui=always', 'branch', ...finalArgs],
+      { stdio: 'inherit' }
+    ).on('exit', code => process.exit(code ?? 0))
+  })
+
+program.parse(process.argv)
